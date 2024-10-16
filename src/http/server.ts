e39d6d1c8bb48bc7053016af0df2146d73d87e82
rjrocha19 <rodolfo_rj@live.com>
2024-10-16 12:03:08 -0300

    Route: Logout and derivation

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,16 +1,16 @@
-import { Elysia, t } from 'elysia'
-import jwt from '@elysiajs/jwt'
+import { Elysia } from 'elysia'
 import cookie from '@elysiajs/cookie'
 import { registerRestaurante } from './routes/register-restaurante'
 import { sendAuthLink } from './routes/send-auth-link'
-import { env } from '../env'
 import { authenticateFromLink } from './routes/authenticate-from-link'
+import { signOut } from './routes/sign-out'
 
 const app = new Elysia()
   .use(cookie())
   .use(registerRestaurante)
   .use(sendAuthLink)
   .use(authenticateFromLink)
+  .use(signOut)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running!')
