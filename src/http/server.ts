caaca5a75c904c7320617d64e11d964c3e00b0f5
rjrocha19 <rodolfo_rj@live.com>
2024-10-17 17:26:39 -0300

    Routes: Actions on the order

--- src/http/server.ts
+++ src/http/server.ts
@@ -7,6 +7,9 @@ import { signOut } from './routes/sign-out'
 import { getProfile } from './routes/get-profile'
 import { getManagedRestaurante } from './routes/get-managed-restaurante'
 import { getOrderDetails } from './routes/get-order-details'
+import { cancelOrder } from './routes/cancel-order'
+import { deliverOrder } from './routes/deliver-order'
+import { dispatchOrder } from './routes/dispatch-order'
 
 const app = new Elysia()
   .use(cookie())
@@ -17,6 +20,9 @@ const app = new Elysia()
   .use(getProfile)
   .use(getManagedRestaurante)
   .use(getOrderDetails)
+  .use(cancelOrder)
+  .use(deliverOrder)
+  .use(dispatchOrder)
   .onError(({ code, error, set }) => {
     switch (code) {
       case 'VALIDATION': {
